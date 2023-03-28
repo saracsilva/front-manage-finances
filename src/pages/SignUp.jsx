1ad4b9f0ac42edcef19a4c8853f35e7355ed6968
saracsilva <saracarolinasilva@gmail.com>
2023-03-28 16:15:59 +0200

    connected the front with the back end

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 
 function SignUp() {
-  return <div>SignUp</div>;
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+  const API_URL = "http://localhost:5005";
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await fetch(`${API_URL}/auth/signup`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ firstName, lastName, email, password }),
+      });
+      const parsed = await response.json();
+    } catch (error) {
+      setErrorMessage(error.message);
+    }
+  };
+  return (
+    <div>
+      <h1>Signup</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          required
+          value={firstName}
+          onChange={(event) => setFirstName(event.target.value)}
+          placeholder="First Name"
+        />
+        <input
+          type="text"
+          required
+          value={lastName}
+          onChange={(event) => setLastName(event.target.value)}
+          placeholder="Last Name"
+        />
+        <input
+          type="email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+        <button type="submit">Signup</button>
+      </form>
+      {errorMessage && <p>{errorMessage}</p>}
+    </div>
+  );
 }
 
 export default SignUp;
